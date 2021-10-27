74a8b255b22b2463e145218ddffa979547a819e5
Myles Moylan <moylanm@oregonstate.edu>
2021-10-27 13:02:20 -0700

    implement list and detail widgets

---
+++ lib/protocol_detail.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'models/protocol.dart';
+import 'styles.dart';
+
+class ProtocolDetail extends StatelessWidget {
+  final Protocol protocol;
+
+  ProtocolDetail(this.protocol);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(protocol.name, style: Styles.navBarTitle)),
+      body: _renderBody(context, protocol),
+    );
+  }
+
+  Widget _renderBody(BuildContext context, Protocol protocol) {
+    return SfPdfViewer.asset(protocol.url);
+  }
+}
---
+++ lib/protocol_list.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'protocol_detail.dart';
+import 'styles.dart';
+import 'models/protocol.dart';
+
+class ProtocolList extends StatelessWidget {
+  final List<Protocol> _protocols;
+
+  ProtocolList(this._protocols);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Protocols',
+          style: Styles.navBarTitle,
+        )
+      ),
+      body: ListView.separated(
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
+        itemCount: _protocols.length,
+        itemBuilder: _listViewItemBuilder,
+      ),
+    );
+  }
+
+  Widget _listViewItemBuilder(BuildContext context, int index) {
+    var protocol = _protocols[index];
+    return ListTile(
+      contentPadding: EdgeInsets.all(10),
+      title: _itemTitle(protocol),
+      onTap: () => _navigationToProtocolDetail(context, _protocols[index])
+    );
+  }
+
+  void _navigationToProtocolDetail(BuildContext context, Protocol protocol) {
+    Navigator.push(context,
+      MaterialPageRoute(builder: (context) => ProtocolDetail(protocol)));
+  }
+
+  Widget _itemTitle(Protocol protocol) {
+    return Center(
+      child: Text(
+        protocol.name,
+        style: Styles.textDefault,
+      )
+    );
+  }
+}

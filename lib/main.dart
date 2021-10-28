e2e010b7aeed0ec3fc20ff3ef9fdfbd27152cd93
Myles Moylan <moylanm@oregonstate.edu>
2021-10-28 15:09:34 -0700

    formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
 import 'home_route.dart';
 
-void main() {
-  return runApp(MaterialApp(home: HomeRoute()));
-}
+void main() => runApp(MaterialApp(home: HomeRoute()));

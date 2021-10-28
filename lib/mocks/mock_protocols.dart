c39f7d2b0b25810adeb6bc00b3f7b33b7e9b23c5
Myles Moylan <moylanm@oregonstate.edu>
2021-10-27 21:58:52 -0700

    move protocol data to map

--- lib/mocks/mock_protocols.dart
+++ lib/mocks/mock_protocols.dart
@@ -1,36 +1,20 @@
 import '../models/protocol.dart';
 
 mixin MockProtocol implements Protocol {
-  static final List<Protocol> _items = [
-    Protocol(
-      name: 'Preface',
-      url: 'assets/Preface.pdf',
-    ),
-    Protocol(
-      name: 'Operations',
-      url: 'assets/Operations.pdf'
-    ),
-    Protocol(
-      name: 'Procedures',
-      url: 'assets/Procedures.pdf',
-    ),
-    Protocol(
-      name: 'Treatment',
-      url: 'assets/Treatment.pdf',
-    ),
-    Protocol(
-      name: 'Medications',
-      url: 'assets/Medications.pdf',
-    ),
-    Protocol(
-      name: 'Trauma System',
-      url: 'assets/Trauma System.pdf',
-    ),
-    Protocol(
-      name: 'Hazardous Materials',
-      url: 'assets/Hazardous Materials.pdf',
-    ),
-  ];
+
+  static const _protocolData = {
+    'Preface': 'assets/Preface.pdf',
+    'Operations': 'assets/Operations.pdf',
+    'Procedures': 'assets/Procedures.pdf',
+    'Treatment': 'assets/Treatment.pdf',
+    'Medications': 'assets/Medications.pdf',
+    'Trauma System': 'assets/Trauma System.pdf',
+    'Hazardous Materials': 'assets/Hazardous Materials.pdf',
+  };
+
+  static final List<Protocol> _items = _protocolData.entries.map(
+    (e) => Protocol(name: e.key, url: e.value)
+  ).toList();
 
   static Protocol fetchAny() {
     return _items[0];

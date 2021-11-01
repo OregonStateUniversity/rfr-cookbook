9a1cd3a4996d3cf5905435c835e7e1b5be74786b
Myles Moylan <moylanm@oregonstate.edu>
2021-11-01 11:04:02 -0700

    files will need to be uploaded with md5 hashes (#11)

--- lib/storage_helper.dart
+++ lib/storage_helper.dart
@@ -1,26 +1,47 @@
 import 'dart:io';
 
+import 'package:crypto/crypto.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path_provider/path_provider.dart';
 
 class StorageHelper {
   static final FirebaseStorage _storageInstance = FirebaseStorage.instance;
 
-  Future<void> initializeLocalStorage() async {
-    ListResult remoteParentDirectories = await _remoteDirectoryList;
+  Future<void> initializeStorage() async {
+    final ListResult remoteParentDirectories = await _remoteDirectoryList;
 
     for (Reference parentDirectory in remoteParentDirectories.prefixes) {
-      ListResult listResult = await _storageInstance.ref(parentDirectory.fullPath).listAll();
-      for (Reference file in listResult.items) {
+      final ListResult directoryList = await _storageInstance.ref(parentDirectory.fullPath).listAll();
+
+      for (Reference file in directoryList.items) {
         _downloadFile(file);
       }
     }
   }
 
+  Future<void> _uploadFileWithMetadata(String localPath, String remotePath) async {
+    File file = File(localPath);
+
+    SettableMetadata metadata = SettableMetadata(
+      customMetadata: <String, String>{'md5Hash': await _generateMd5(localPath)}
+    );
+
+    try {
+      await _storageInstance.ref(remotePath).putFile(file, metadata);
+    } on FirebaseException catch (e) {
+      throw e.code;
+    }
+  }
+
+  Future<String> _generateMd5(String pathToFile) async {
+    final fileStream = File(pathToFile).openRead();
+    return (await md5.bind(fileStream).first).toString();
+  }
+
   Future<void> _downloadFile(Reference ref) async {
-    String appDocDir = await _localRootPath;
-    File downloadToFile = File('$appDocDir/${ref.fullPath}');
-    
+    final String appDocDir = await _localRootPath;
+    final File downloadToFile = File('$appDocDir/${ref.fullPath}');
+
     try {
       await _storageInstance
         .ref(ref.fullPath)

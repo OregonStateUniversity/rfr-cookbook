e13a11eb84fa400f7954f2c8b9558e2d943c49dc
Myles Moylan <moylanm@oregonstate.edu>
2021-11-04 11:44:54 -0700

    add dropdown logout button

--- lib/screens/admin_panel.dart
+++ lib/screens/admin_panel.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:rfr_cookbook/styles.dart';
 
@@ -10,9 +11,40 @@ class AdminPanel extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Admin Panel', style: Styles.navBarTitle),
         backgroundColor: Styles.navBarColor,
+        actions: [
+          _renderPopupMenuButton(context),
+        ],
       ),
       body: const Text('Admin Panel'),
     );
+  }
+
+  PopupMenuButton _renderPopupMenuButton(BuildContext context) {
+    return PopupMenuButton(
+      onSelected: (item) => _selectedItem(context, item as int),
+      itemBuilder: (context) =>
+      [
+        PopupMenuItem(
+          value: 0,
+          child: Row(
+            children: const [
+              Icon(Icons.logout, color: Colors.red),
+              SizedBox(width: 7),
+              Text('Logout')
+            ],
+          )
+        )
+      ]
+    );
+  }
+
+  void _selectedItem(BuildContext context, int item) {
+    switch (item) {
+      case 0: // logout
+        FirebaseAuth.instance.signOut();
+        Navigator.of(context).pop();
+        break;
+    }
   }
   
 }

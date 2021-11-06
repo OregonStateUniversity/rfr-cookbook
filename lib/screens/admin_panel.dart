7f4f3f8f85c0106bcfe67ac0f8346a83fc4ae713
Myles Moylan <moylanm@oregonstate.edu>
2021-11-05 18:39:02 -0700

    move logout functionality to corner dropdown menu

--- lib/screens/admin_panel.dart
+++ lib/screens/admin_panel.dart
@@ -1,9 +1,27 @@
+import 'dart:io';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:rfr_cookbook/storage_helper.dart';
 import 'package:rfr_cookbook/styles.dart';
 
-class AdminPanel extends StatelessWidget {
+const FooterHeight = 100.0;
+
+class AdminPanel extends StatefulWidget {
   const AdminPanel({Key? key}) : super(key: key);
+
+  @override
+  _AdminPanelState createState() => _AdminPanelState();
+}
+
+class _AdminPanelState extends State<AdminPanel> {
+  final StorageHelper _storageHelper = StorageHelper();
+  Map<String, List<File>> _protocolDirectories = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFiles();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,32 +29,62 @@ class AdminPanel extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Admin Panel', style: Styles.navBarTitle),
         backgroundColor: Styles.navBarColor,
+        actions: [
+          _renderPopupMenu(context)
+        ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-            child: ElevatedButton(
-              style: Styles.buttonStyle,
-              child: Text('Logout', style: Styles.buttonText),
-              onPressed: () {
-                _logout(context);
-              },
-            ),
-          )
-        ]
-      )
+      body: _renderBody(context)
     );
   }
 
-  void _logout(BuildContext context) {
+  Future<void> _loadFiles() async {
+    final files = await _storageHelper.directoryMap();
+
+    if (mounted) {
+      setState(() {
+        _protocolDirectories = files as Map<String, List<File>>;
+      });
+    }
+  }
+
+  Widget _renderPopupMenu(BuildContext context) {
+    return PopupMenuButton(
+      onSelected: (item) => _selectedItem(context, item as int),
+      itemBuilder: (context) => [
+        PopupMenuItem(
+          value: 0,
+          child: Row(
+            children: const [
+              Icon(Icons.logout, color: Colors.red),
+              SizedBox(width: 7),
+              Text('Logout')
+            ])
+        )
+      ]
+    );
+  }
+
+  Widget _renderBody(BuildContext context) {
+    return const Text('Admin Panel Body');
+  }
+
+  void _selectedItem(BuildContext context, int item) {
+    switch (item) {
+      case 0: // logout
+        _handleLogout(context);
+        break;
+      default:
+    }
+  }
+
+  void _handleLogout(BuildContext context) {
     FirebaseAuth.instance.signOut();
     Navigator.of(context).pop();
     ScaffoldMessenger.of(context)
       .showSnackBar(
-        const SnackBar(
-          content: Text('You have been logged out.', textAlign: TextAlign.center)
+        SnackBar(
+          backgroundColor: Colors.black.withOpacity(0.5),
+          content: const Text('You have been logged out.', textAlign: TextAlign.center)
         )
       );
   }

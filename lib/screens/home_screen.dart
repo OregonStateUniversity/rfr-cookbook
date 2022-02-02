0a2aa822fff975364455fc3fa0fbaf9a340c2182
Adrianna Gilmore <a_inman33@yahoo.com>
2022-02-01 23:37:11 -0800

    got rid of hard coded list of search options

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -46,16 +46,12 @@ class _HomeScreenState extends State<HomeScreen> {
             IconButton(
                 icon: const Icon(Icons.search),
                 onPressed: () async {
-                  //final finalResult = await
                   showSearch(
                       context: context,
                       delegate: SearchBar(
                           allSearchResults: searchList(context),
                           searchSuggestions: searchList(context),
                           storedItemList: storedItemList(context)));
-                  //setState(() {
-                  //selectedResult = finalResult!;
-                  //});
                 }),
           ],
         ),
@@ -127,75 +123,3 @@ class _HomeScreenState extends State<HomeScreen> {
     return fileList;
   }
 }
-
-final titleList = [
-  //0-Preface
-  "00-010 Scope of Practice",
-  "00-020 Advance Directives",
-  "00-030 Death in the Field",
-  "00-040 Glasgow Coma Scale",
-  "00-050 Medical Control for Drugs & Procedures",
-  "00-060 Universal Patient Care",
-  "00-070 BLS Guidelines for single role EMT's",
-  //1-Treatment
-  "10-010 Abdominal Pain",
-  "10-020 Altered Mental Status",
-  "10-030 Anaphylaxis",
-  "10-040 Burns",
-  "10-050 Cardiac Arrest - AED-CPR-HP CPR",
-  "10-051 Cardiac Arrest - Asystole",
-  "10-052 Cardiac Arrest - PEA",
-  "10-053 Cardiac Arrest - VF_Pulseless VT",
-  "10-054 Cardiac Arrest - Post Resuscitation",
-  "10-060 Cardiac Dysrhythmias - Bradycardia",
-  "10-061 Cardiac Dysrhythmias - Tachycardia",
-  "10-070 Chest Pain ACS",
-  "10-080 Crush Injury",
-  "10-090 Eye Emergencies",
-  "10-100 Hyperkalemia",
-  "10-110 Hypertension",
-  "10-120 Hyperthermia",
-  "10-130 Hypothermia",
-  "10-140 Musculoskeletal Extremity Trauma",
-  "10-141 Musculoskeletal Spinal Injury",
-  "10-150 Nausea and Vomiting",
-  "10-160 Neonatal Resuscitation",
-  "10-170 OB GYN Emergencies",
-  "10-180 Pain Management",
-  "10-190 Poisoning and Overdose",
-  "10-200 Respiratory Distress",
-  "10-210 Seizures",
-  "10-220 Shock",
-  "10-230 Snakebike",
-  "10-240 Stroke-CVA",
-  "10-250 Submerged Patient",
-  "10-260 Traumatic Brain Injury",
-  //2-Medications
-  "20-010 Acetaminophen",
-  "20-020 Activated Carcoal",
-  "20-030 Adenosine",
-  "20-040 Albuterol",
-  "20-041 Albuterol & Altrovent",
-  "20-050 Amiodarone",
-  "20-060 Asprin",
-  "20-070 Atrophine Sulfate",
-  "20-080 Calcium Chloride 10-",
-  "20-081 Calcium Gluconate",
-  "20-090 Dextrose",
-  "20-100 Diltiazem",
-  "20-110 Diphenhydramine",
-  "20-120 Dopamine",
-  "20-130 Epinephrine",
-  //3-Procedures
-  //4-Operations
-  //5-Trauma
-  //6-Hazmat
-  //7-Misc
-];
-
-final suggestionList = [
-  "10-180 Pain Management",
-  "10-020 Altered Mental Status",
-  "10-190 Poisoning and Overdose",
-  "10-150 Nausea and Vomiting",
-];

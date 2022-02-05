5b08954961a14bc8cbdf28ece41f65d70d5f5585
Myles Moylan <moylanm@oregonstate.edu>
2022-02-05 11:04:38 -0800

    add uploadedAt attribute (#30)

--- lib/models/stored_item.dart
+++ lib/models/stored_item.dart
@@ -3,11 +3,13 @@ import 'package:firebase_storage/firebase_storage.dart';
 
 class StoredItem {
   final String name;
+  final String uploadedAt;
   final File localFile;
   final Reference remoteReference;
 
   StoredItem({
     required this.name,
+    required this.uploadedAt,
     required this.localFile,
     required this.remoteReference
   });

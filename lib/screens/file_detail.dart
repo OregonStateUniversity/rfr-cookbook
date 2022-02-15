6ff80862a28f13d39924c2d09f4a4e694a07f9ab
Adrianna Gilmore <a_inman33@yahoo.com>
2022-02-14 22:36:21 -0800

    #32 added search button to file_detail

--- lib/screens/file_detail.dart
+++ lib/screens/file_detail.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:rfr_cookbook/models/stored_item.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:rfr_cookbook/config/styles.dart';
+import 'package:rfr_cookbook/search.dart';
+import 'package:rfr_cookbook/storage_helper.dart';
 
 class FileDetail extends StatelessWidget {
   final StoredItem _file;
@@ -14,8 +16,35 @@ class FileDetail extends StatelessWidget {
       appBar: AppBar(
         title: Text(_file.name, style: Styles.navBarTitle),
         backgroundColor: Styles.themeColor,
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () async {
+                showSearch(
+                    context: context,
+                    delegate: SearchBar(
+                        allSearchResults: _searchList(context),
+                        searchSuggestions: _searchList(context),
+                        storedItemList: _storedItemList(context)));
+              }),
+        ],
       ),
       body: SfPdfViewer.file(_file.localFile),
     );
+  }
+
+  List<String> _searchList(BuildContext context) {
+    List<String> list =
+        _storedItemList(context)!.map((file) => file.name).toList();
+    return list;
+  }
+
+  List<StoredItem>? _storedItemList(BuildContext context) {
+    final StorageHelper _storageHelper = StorageHelper();
+    List<StoredItem> pdfList = [];
+    for (int i = 0; i <= 7; i++) {
+      pdfList += _storageHelper.localStorageMap.values.toList()[i];
+    }
+    return pdfList;
   }
 }

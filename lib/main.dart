61ef8c94f62c307adfe1581294b16fbc220ff11b
Myles Moylan <moylanm@oregonstate.edu>
2022-02-20 18:28:44 -0800

    main function does not need to be asynchronous

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(FutureBuilder(

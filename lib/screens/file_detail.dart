65cc65c02c3e90b64f0a259532b3cfe09095e194
Adrianna Gilmore <a_inman33@yahoo.com>
2022-02-25 12:22:17 -0800

    having some issues but added code for pdf search

--- lib/screens/file_detail.dart
+++ lib/screens/file_detail.dart
@@ -4,6 +4,7 @@ import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:rfr_cookbook/config/styles.dart';
 import 'package:rfr_cookbook/search.dart';
 import 'package:rfr_cookbook/storage_helper.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
 
 class FileDetail extends StatefulWidget {
   final StoredItem _file;
@@ -14,9 +15,15 @@ class FileDetail extends StatefulWidget {
 }
 
 class _FileDetailState extends State<FileDetail> {
-  //final StoredItem _file;
+  late PdfViewerController _pdfViewerController;
+  late PdfTextSearchResult _searchResult;
 
-  //const FileDetail(this._file, {Key? key}) : super(key: key);
+  @override
+  void initState() {
+    _pdfViewerController = PdfViewerController();
+    _searchResult = PdfTextSearchResult();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,23 +31,36 @@ class _FileDetailState extends State<FileDetail> {
       appBar: AppBar(
         title: Text(widget._file.name, style: Styles.navBarTitle),
         backgroundColor: Styles.themeColor,
-        actions: [
+        actions: <Widget>[
           IconButton(
               icon: const Icon(Icons.search),
               onPressed: () async {
+                print("we are getting here");
+                _searchResult = await _pdfViewerController.searchText('pain',
+                    searchOption: TextSearchOption.caseSensitive);
+                if (_searchResult.totalInstanceCount == 0) {
+                  print('No matches found.');
+                }
+                print(
+                    'Total instance count: ${_searchResult.totalInstanceCount}');
+                setState(() {});
+
+                /*
                 showSearch(
                     context: context,
                     delegate: SearchBar(
                         allSearchResults: _searchList(context),
                         searchSuggestions: _searchList(context),
                         storedItemList: _storedItemList(context)));
+              */
               }),
         ],
       ),
-      body: SfPdfViewer.file(widget._file.localFile),
+      body: SfPdfViewer.file(widget._file.localFile,
+          controller: _pdfViewerController),
     );
   }
-
+/*
   List<String> _searchList(BuildContext context) {
     List<String> list =
         _storedItemList(context)!.map((file) => file.name).toList();
@@ -54,5 +74,5 @@ class _FileDetailState extends State<FileDetail> {
       pdfList += _storageHelper.localStorageMap.values.toList()[i];
     }
     return pdfList;
-  }
+  }*/
 }

78df7c2a5e2c8c20cb675a254dbb40e0bf39d69c
Adrianna Gilmore <adriannagilmore@10-116-11-39.wireless.oregonstate.edu>
2022-04-01 11:05:32 -0700

    pushing search changes

--- lib/screens/file_detail.dart
+++ lib/screens/file_detail.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:rfr_cookbook/models/stored_item.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:rfr_cookbook/config/styles.dart';
-import 'package:rfr_cookbook/search.dart';
-import 'package:rfr_cookbook/storage_helper.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 
 class FileDetail extends StatefulWidget {
@@ -35,7 +33,6 @@ class _FileDetailState extends State<FileDetail> {
           IconButton(
               icon: const Icon(Icons.search),
               onPressed: () async {
-                print("we are getting here");
                 _searchResult = await _pdfViewerController.searchText('pain',
                     searchOption: TextSearchOption.caseSensitive);
                 if (_searchResult.totalInstanceCount == 0) {
@@ -44,15 +41,6 @@ class _FileDetailState extends State<FileDetail> {
                 print(
                     'Total instance count: ${_searchResult.totalInstanceCount}');
                 setState(() {});
-
-                /*
-                showSearch(
-                    context: context,
-                    delegate: SearchBar(
-                        allSearchResults: _searchList(context),
-                        searchSuggestions: _searchList(context),
-                        storedItemList: _storedItemList(context)));
-              */
               }),
         ],
       ),
@@ -60,19 +48,4 @@ class _FileDetailState extends State<FileDetail> {
           controller: _pdfViewerController),
     );
   }
-/*
-  List<String> _searchList(BuildContext context) {
-    List<String> list =
-        _storedItemList(context)!.map((file) => file.name).toList();
-    return list;
-  }
-
-  List<StoredItem>? _storedItemList(BuildContext context) {
-    final StorageHelper _storageHelper = StorageHelper();
-    List<StoredItem> pdfList = [];
-    for (int i = 0; i <= 7; i++) {
-      pdfList += _storageHelper.localStorageMap.values.toList()[i];
-    }
-    return pdfList;
-  }*/
 }
